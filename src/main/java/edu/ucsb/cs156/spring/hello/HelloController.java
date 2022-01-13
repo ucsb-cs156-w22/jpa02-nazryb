eb1c35eb011f2a061ecf69f4f24eebd2ffdb9663
Nazar R <nrybii@umail.ucsb.edu>
2022-01-12 21:48:22 -0800

    change the test so it passes

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from nrybii";
+        return "Greetings from nrybii!";
     }
     
 }
